4f337746b549eac5f44244786f28e8f31574f4c8
arnbonnet <arnaud.bonnet@etu.u-bordeaux.fr>
2017-01-17 19:09:43 +0100

    initial commit

---
+++ sorcery.js
@@ -0,0 +1,56 @@
+$( function() {
+	var buttons = $(".section button");
+	var status = $("#status");
+	var life = $("#status > .life > .value");
+
+	gotoSection("intro");
+	// $(".section").hide();
+	// $("#intro").show();
+	
+	buttons.click( function() {
+		if($(this).attr("go") == "hitWall" || $(this).attr("go") == "hitDoor") {
+			loseOneLife();
+		}
+
+		if(life.text() == "0") {
+				endGame();
+		}
+		else {
+			gotoSection($(this).attr("go"));
+		}
+
+	} );
+	
+	function gotoSection(key) {
+		$(".section").hide();
+		$("#" + key).show();
+		if("intro" == key)
+			setLife("--");
+		if("wakeUp" == key)
+			startGame();
+	}
+	
+	function getLife() {
+		life.text();
+	}
+	
+	function setLife(v) {
+		life.text(v);
+	}
+	
+	function loseOneLife() {
+		setLife(life.text()-1);
+	}
+	
+	function startGame() {
+		setLife(3);
+	}
+	
+	function endGame() {
+		gotoSection("death");
+		buttons.click( function() {
+			gotoSection($(this).attr("go"));
+		});
+	}
+	
+} );
