0454b1f9e868abb96bf0e777e224f343c4aee02c
Chezardy <arno33370@hotmail.fr>
2017-01-28 17:19:17 +0100

    added life/alert system + game over

--- thief.js
+++ thief.js
@@ -1,52 +1,84 @@
 $( function() {
 	var buttons = $(".section button");
 	var status = $("#status");
-	var life = $("#status > .life > .value");
+	var life = $("#status > .life > .lifeValue");
+	var alert = $("#status > .alert > .alertValue");
 
 	gotoSection("intro");
-	
+
 	buttons.click( function() {
-		if($(this).attr("go") == "hitWall" || $(this).attr("go") == "hitDoor") {
-			loseOneLife();
+		key = $(this).attr("go");
+
+		if( key == "roof" || key == "window" 
+			|| key == "eatingCat" || key == "failedVault" 
+			|| key == "takeTabletCat")
+		{
+			looseLife(1);
 		}
 
-		if(life.text() == "0") {
-				endGame();
-		}
-		else {
-			gotoSection($(this).attr("go"));
+		if(key == "roof")
+			increaseAlert(2);
+
+		if(key == "garden" || key == "window" 
+			|| key == "eatingCat" || key == "drawerOnFloor" 
+			|| key == "failedVault" || key == "takeTabletCat" 
+			|| key == "successWardrobe")
+		{
+			increaseAlert(1);
 		}
 
-	} );
+		gotoSection(key);
+
+		if(life.text() == "0" || alert.text() == "4")
+			endGame(key);
+
+	});
 	
 	function gotoSection(key) {
 		$(".section").hide();
 		$("#" + key).show();
-		if("intro" == key)
+		if("intro" == key) {
 			setLife("--");
+			setAlert("--");
+		}
 		if("manoir" == key)
 			startGame();
-	}
-	
-	function getLife() {
-		life.text();
 	}
 	
 	function setLife(v) {
 		life.text(v);
 	}
 	
-	function loseOneLife() {
-		setLife(life.text()-1);
+	function setAlert(a) {
+		alert.text(a);
+	}
+	
+	function looseLife(v) {
+		setLife(parseInt(life.text())-v);
+	}
+
+	function increaseAlert(a) {
+		setAlert(parseInt(alert.text())+a);
 	}
 	
 	function startGame() {
 		setLife(3);
+		setAlert(0);
 	}
 	
-	function endGame() {
-		gotoSection("death");
-		buttons.click( function() {
+	function endGame(key) {
+		//remove buttons of this section
+		$("#" + key + " button").remove();
+
+		if(life.text() == "0")
+			$("#" + key).append("Vous vous êtes évanouis.<br />Vous êtes à présent au poste de police.");
+		else if(alert.text() == "4")
+			$("#" + key).append("La police surgit dans la pièce et vous arrête.");
+
+		//add restart button
+		$("#" + key).append("<button go='intro'>Recommencer.</button>");
+		//allows to click on it
+		$("#" + key + " button").click(function() {
 			gotoSection($(this).attr("go"));
 		});
 	}
