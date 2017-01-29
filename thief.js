f0013b6f931ac4667ec65a0531f6d919f3079cbf
Chezardy <arno33370@hotmail.fr>
2017-01-29 18:45:32 +0100

    added consequences of life and alert + added tools system

--- thief.js
+++ thief.js
@@ -3,10 +3,32 @@ $( function() {
 	var status = $("#status");
 	var life = $("#status > .life > .lifeValue");
 	var alert = $("#status > .alert > .alertValue");
+	var tools = $("#status > .inventory > .toolsValue");
 
 	gotoSection("intro");
 
 	buttons.click( function() {
+
+		//CONSEQUENCES OF ALERT
+		if(parseInt(alert.text()) > 2) {
+			$("#kitchen > button[go='emptyKitchen']").attr("go", "eatingCat");
+		}
+
+		//CONSEQUENCES OF LIFE
+		if(parseInt(life.text()) == 1) {
+			$("#cutlery > button[go='drawerOnFloor']").attr("go", "failedDrawer");
+		}
+
+		//TOOLS SYSTEM
+		if(parseInt(tools.text()) > 0) {
+			$(".tools").show();
+		} else {
+			$(".tools").hide();
+		}
+		if($(this).attr("class") == "tools")
+			looseTools(1);
+
+		//LIFE AND ALERT SYSTEM
 		key = $(this).attr("go");
 
 		if( key == "roof" || key == "window" 
@@ -33,15 +55,13 @@ $( function() {
 			endGame(key);
 
 	});
-	
+
+
 	function gotoSection(key) {
 		$(".section").hide();
 		$("#" + key).show();
-		if("intro" == key) {
-			setLife("--");
-			setAlert("--");
-		}
-		if("manoir" == key)
+
+		if("intro" == key)
 			startGame();
 	}
 	
@@ -51,6 +71,14 @@ $( function() {
 	
 	function setAlert(a) {
 		alert.text(a);
+	}
+
+	function setTools(t) {
+		tools.text(t);
+	}
+
+	function looseTools(t) {
+		setTools(parseInt(tools.text())-t);
 	}
 	
 	function looseLife(v) {
@@ -64,6 +92,7 @@ $( function() {
 	function startGame() {
 		setLife(3);
 		setAlert(0);
+		setTools(1);
 	}
 	
 	function endGame(key) {
